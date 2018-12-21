36dba4e4437e3e561230a40cce7a3950bc2aa992
Tan Wei Shen <tanweishen97@gmail.com>
2018-12-21 15:52:58 +0800

    Dialog adjustments
    Changed Player class interaction with Dialog class

--- sandbox/dialog/t1.py
+++ sandbox/dialog/t1.py
@@ -4,17 +4,19 @@ from pygame import *
 SCREEN_SIZE = pygame.Rect((0, 0, 800, 640))
 
 class Dialog():
-    def __init__(self, pos):
+    def __init__(self, pos, surface):
         self.dialogues = dict()
         self.image = Surface((SCREEN_SIZE.width, SCREEN_SIZE.height))
         self.image.fill(Color("#800080"))
         self.rect = self.image.get_rect(topleft=pos)
         self.hasControl = False
         self.hide = True # change to self.visible
+        self.textFont = pygame.font.SysFont("Comic Sans", 35)
+        self.screen = surface
 
     #add hide() and show() functions.
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
+    def draw(self):
+        self.screen.blit(self.image, self.rect)
 
     def update(self):
         pressed = pygame.key.get_pressed()
@@ -30,8 +32,44 @@ class Dialog():
         #update texts in the dialog here i guess?
         return
 
-    def displayDialogue(self):
-        return None
+    def loadText(self, key):
+        
+
+        rowLength = 75
+        text = self.dialogues[key]
+        print(len(text))
+        
+        n = len(text)
+        start = 0
+        end = 0
+        rowCounter = 0
+        while n > rowLength:
+            #find the end.
+            for k in range (rowLength+start-1, start, -1):
+                if text[k] == " ":
+                    end = k
+                    break
+            print(len(text[start:end]))
+            textSurface = self.textFont.render(text[start:end], True, (255,255,255))
+            self.image.blit(textSurface, (0,rowCounter))
+
+            rowCounter += 32
+
+            truncatedTextLength = end - start + 1
+            n -= truncatedTextLength
+            
+            start = end + 1
+        #add remainder to next row.
+        textSurface = self.textFont.render(text[start:len(text)], True, (255,255,255))
+        self.image.blit(textSurface, (0, rowCounter))
+            
+
+            
+        
+        
+        
+        print(text)
+
         
         
 
@@ -60,6 +98,9 @@ class Dialog():
                         for k in range (len(line)):
                             currText += line[k]
 
+                            if k != len(line)-1:
+                                currText += " "
+
         for i in range (len(keys)):
             self.dialogues[keys[i]] = texts[i]
 
@@ -69,16 +110,29 @@ class Dialog():
 
 
 def main():
+
     pygame.init()
+    #init font
+    pygame.font.init()            
     screen = pygame.display.set_mode((800, 640))
     timer = pygame.time.Clock()
-    gameDialog = Dialog((0, 442))
+    gameDialog = Dialog((0, 442), screen)
     gameDialog.read("dialogues.txt")
     for key, val in gameDialog.dialogues.items():
         print("----")
         print("KEY: " + str(key))
         print("Value: ")
         print(val)
+        
+    print("Text of plant 1.0: " )
+    #gameDialog.displayDialogue("-plant1.0-")
+
+    print(" " )
+
+    print("text of -intro1.1-")
+    gameDialog.loadText("-intro1.1-")
+    
+        
         
     while True:
         for e in pygame.event.get():
@@ -98,7 +152,7 @@ def main():
 
         gameDialog.update()
         if not gameDialog.hide:
-            gameDialog.draw(screen)
+            gameDialog.draw()
             
 
         pygame.display.update()
